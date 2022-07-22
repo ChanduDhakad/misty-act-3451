fd298a275a1be9877f93bd4eab98ac277486399b
Ninadjoshi212 <ninadjoshi212@gmail.com>
2022-07-23 02:49:53 +0530

    search function and css added

---
+++ HeaderNFooter/InnersiteNav/components/innerNav.js
@@ -0,0 +1,28 @@
+// import innernav from "./navbarComp.js";
+// import foo from "../../homepageNavbar/Components/footer.js"
+
+// let nav=document.getElementById('header');
+// nav.innerHTML=innernav();
+// let footer=document.getElementById('footer');
+// footer.innerHTML=foo();
+
+// let hamburger = document.getElementById("hamburger");
+// let timesClicked = 0;
+
+// let funcDispBlock = () => {
+//   document.getElementById("categories").style.display = "flex";
+//   document.getElementById("img").style.display = "none";
+// };
+// let funcDispNone = () => {
+//   document.getElementById("categories").style.display = "none";
+//   document.getElementById("img").style.display = "block";
+// };
+
+// hamburger.addEventListener("click", function () {
+//   timesClicked++;
+//   if (timesClicked % 2 == 0) {
+//     funcDispBlock();
+//   } else {
+//     funcDispNone();
+//   }
+// });
---
+++ HeaderNFooter/InnersiteNav/components/navbarComp.js
@@ -0,0 +1,43 @@
+let innernav = () => {
+    return `
+    <div class="container">
+      <div id="toggle">
+        <div id="hamburger"><i class="fa-solid fa-2x fa-bars"></i></div>
+        <div id="img">
+          <div id="logodiv">
+            <a href="index.html"
+              ><img
+                id="logo"
+                src="https://cdn2.stylecraze.com/wp-content/themes/buddyboss-child/images/sc-logo.png"
+                alt="logo"
+            /></a>
+          </div>
+        </div>
+        <div id="categories">
+          <div class="child">
+            <div id="cate1">MAKEUP</div>
+            <div id="cate2">HAIR CARE</div>
+            <div id="cate3">SKIN CARE</div>
+            <div id="cate4">HAIRSTYLES</div>
+            <div id="cate5">HEALTH & WELLNESS</div>
+            <div id="cate6">NEWS</div>
+            <div>TOOLS</div>
+            <div>PRODUCTS</div>
+            <div id="cate7">ABOUT US</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+}
+
+let funcDispBlock = () => {
+  document.getElementById("categories").style.display = "flex";
+  document.getElementById("img").style.display = "none";
+};
+let funcDispNone = () => {
+  document.getElementById("categories").style.display = "none";
+  document.getElementById("img").style.display = "block";
+};
+
+export {innernav, funcDispBlock,funcDispNone};
